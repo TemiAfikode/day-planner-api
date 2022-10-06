92c50f62de7aeb3eb9ae09f50137a3fcfd08d855
TemiAfikode <babalolaife@yahoo.com>
2022-10-06 15:38:33 +0100

    remove static file

--- index.js
+++ index.js
@@ -44,14 +44,6 @@ app.use("/api/tasks", taskRoutes);
 
 app.use("*", mongoValidation);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 const port = process.env.PORT || 9000;
 
 app.listen(port, () => {
