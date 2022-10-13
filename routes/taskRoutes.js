c3875eafb7c55e3fda4d346fe19c21c98c1be9c1
TemiAfikode <babalolaife@yahoo.com>
2022-10-13 21:02:52 +0100

    remove pro account middleware

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,25 +1,27 @@
-const express = require('express');
-const addTaskController = require('../controllers/task/addTaskController');
-const deleteTaskController = require('../controllers/task/deleteTaskController');
-const getTaskController = require('../controllers/task/getTaskController');
-const getTasksController = require('../controllers/task/getTasksController');
-const getUserTaskController = require('../controllers/task/getUserTaskController');
-const shareTaskController = require('../controllers/task/shareTaskController');
-const updateTaskController = require('../controllers/task/updateTaskController');
-const authorize = require('../middleware/authorize');
-const {taskCap, noSharing} = require('../middleware/basicRestrictions');
+const express = require("express");
+const addTaskController = require("../controllers/task/addTaskController");
+const deleteTaskController = require("../controllers/task/deleteTaskController");
+const getTaskController = require("../controllers/task/getTaskController");
+const getTasksController = require("../controllers/task/getTasksController");
+const getUserTaskController = require("../controllers/task/getUserTaskController");
+const shareTaskController = require("../controllers/task/shareTaskController");
+const updateTaskController = require("../controllers/task/updateTaskController");
+const authorize = require("../middleware/authorize");
+// const {taskCap, noSharing} = require('../middleware/basicRestrictions');
 
 const router = express.Router();
 
-router.post('/share/:id',authorize, noSharing,  shareTaskController)
-router.get('/my-tasks/:userId',authorize, getUserTaskController)
+router.post("/share/:id", authorize, shareTaskController);
+router.get("/my-tasks/:userId", authorize, getUserTaskController);
 
-router.route('/')
-    .get(authorize, getTasksController)
-    .post(authorize, addTaskController)
-router.route('/:id')
-    .get(authorize, getTaskController)
-    .delete(authorize, deleteTaskController)
-    .put(authorize, updateTaskController)
+router
+  .route("/")
+  .get(authorize, getTasksController)
+  .post(authorize, addTaskController);
+router
+  .route("/:id")
+  .get(authorize, getTaskController)
+  .delete(authorize, deleteTaskController)
+  .put(authorize, updateTaskController);
 
-module.exports = router
+module.exports = router;
